58539366b26fb42384261cd8c80089fad7baac4a
samueltheuri <samueltheuri695@gmail.com>
2022-09-12 16:54:43 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main -Entry point
+ * Description: prints allsingle digit numbers of
+ * base 10 from 0
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; < 10; a++)
+	{
+		printf("%d", a);
+
+	}
+	putcher('\n');
+	return (0);
+}
